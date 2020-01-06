2cf3f82e9daf8dae1ed285bbb83a13912643aef7
Jackie Ouzman <jackie.ouzman@csiro.au>
2020-01-06 16:44:16 +1030

    looking good still need to check a couple of things

--- Dunns_strip_graphs.R
+++ Dunns_strip_graphs.R
@@ -138,7 +138,7 @@ Dunn_strips_plotting$name_strip <- factor(Dunn_strips_plotting$name_strip,
                                          "100MESZ"
                                           ))
 
-
+unique(Dunn_strips_plotting$name_strip)
 
 ggplot(Dunn_strips_plotting, aes(DistOnLine, value, group = name_strip))+
   geom_line(size=2, alpha=0.4, aes( color = name_strip ))+
@@ -155,10 +155,16 @@ ggplot(Dunn_strips_plotting, aes(DistOnLine, value, group = name_strip))+
   geom_point(data = filter(Dunn_strips_plotting, zone == 1), aes(DistOnLine, value), shape=1, size =2)+
   geom_point(data = filter(Dunn_strips_plotting, zone == 2), aes(DistOnLine, value), shape=1, size =2)
 
+#what is the average yield for each treatment
+average_yld_per_sample_zone <- group_by(Dunn_strips_plotting, name_strip, zone) %>% 
+  summarise(average = mean(value))
 
+print(average_yld_per_sample_zone)
 
+average_yld_per_treatmnet <- group_by(Dunn_strips_plotting, name_strip) %>% 
+  summarise(average = mean(value))
 
-
+print(average_yld_per_treatmnet)
 
 
 # make a clm for rate of P applied and N applied
@@ -179,7 +185,7 @@ Dunn_strips_partialGM <- Dunn_strips_plotting %>%
          )
 
 cost_MESZ = 690 #$/t
-cost_urea = 45 #$/t
+cost_urea = 455 #$/t
 port_price = 250
 Dunn_strips_partialGM <- Dunn_strips_partialGM %>% 
   mutate(
@@ -190,6 +196,27 @@ Dunn_strips_partialGM <- Dunn_strips_partialGM %>%
     partial_GM = revenue - fert_cost
     )
 
+
+Dunn_strips_partialGM$name_strip <- factor(Dunn_strips_partialGM$name_strip, 
+                                          levels = c("0MESZ",
+                                                     "25MESZ", 
+                                                     "50MESZ",
+                                                     "100MESZ"
+                                          ))
+
+unique(Dunn_strips_partialGM$name_strip)
+str(Dunn_strips_partialGM)
+
+#what is the average partial GM for each treatment
+average_PGM_per_sample_zone <- group_by(Dunn_strips_partialGM, name_strip, zone) %>% 
+  summarise(average = mean(partial_GM))
+
+print(average_PGM_per_sample_zone)
+
+average_PGM_per_treatmnet <- group_by(Dunn_strips_partialGM, name_strip) %>% 
+  summarise(average = mean(partial_GM))
+
+print(average_PGM_per_treatmnet)
 
 
 
@@ -209,3 +236,52 @@ ggplot(Dunn_strips_partialGM, aes(DistOnLine, partial_GM, group = name_strip))+
   #scale_y_continuous(labels = scales::percent_format(accuracy = 5L))
   geom_point(data = filter(Dunn_strips_partialGM, zone == 1), aes(DistOnLine, partial_GM), shape=1, size =2)+
   geom_point(data = filter(Dunn_strips_partialGM, zone == 2), aes(DistOnLine, partial_GM), shape=1, size =2)
+
+
+
+########################################################################################################
+
+
+##Read in file
+name_of_path <-
+  file.path("W:", "value_soil_testing_prj", "Yield_data", "Dunns", "Raw_data",  "AndrewParsons_Dunns_23Nov2019_extracted.csv")
+Dunn_raw_extracted <-read.csv(name_of_path)
+str(Dunn_raw_extracted)
+Dunn_raw_extracted <- select(Dunn_raw_extracted,
+                             ID,Zone,Treatment,
+                             Yld = Yld.Mass.Dry..tonne.ha.
+                             )
+Dunn_raw_extracted_summary <- group_by(Dunn_raw_extracted, ID, Treatment) %>% 
+  summarise(value = mean(Yld))
+
+print(Dunn_raw_extracted_summary)
+
+Dunn_raw_extracted_summary_partialGM <- Dunn_raw_extracted_summary %>% 
+  mutate(
+    p_applied = case_when(
+       Treatment == "MESZ 0" ~ 0,
+       Treatment == "MESZ 25" ~ 25,
+      Treatment == "MESZ 50" ~ 50,
+      Treatment == "MESZ 100" ~ 100),
+    n_applied = case_when(
+      Treatment == "MESZ 0" ~ 45 + 75, # top dressed with 75 on the 28/6
+      Treatment == "MESZ 25" ~ 45+ 75,
+      Treatment == "MESZ 50" ~ 45 +75,
+      Treatment == "MESZ 100" ~ 45+ 75)
+  )
+print(Dunn_raw_extracted_summary_partialGM)
+
+Dunn_raw_extracted_summary_partialGM <- Dunn_raw_extracted_summary_partialGM %>% 
+  mutate(
+    p_cost = ((p_applied/1000) * cost_MESZ),
+    n_cost = ((n_applied/1000) * cost_urea),
+    fert_cost = p_cost + n_cost,
+    revenue = value * port_price,
+    partial_GM = revenue - fert_cost
+  )
+
+Dunn_raw_extractedpartialGM <- group_by(Dunn_raw_extracted_summary_partialGM, ID) %>% 
+  summarise(average = mean(partial_GM))
+
+print(Dunn_raw_extractedpartialGM)
+
